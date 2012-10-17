79c7ff6fd85dca0eaeaf8ba98d2a4e7acb5f62ac
Buu Nguyen <buunguyen@gmail.com>
2012-10-17 15:50:06 -0700

    Update

--- tasks/mindirect.js
+++ tasks/mindirect.js
@@ -1,13 +1,13 @@
 module.exports = function(grunt) {
-  grunt.registerMultiTask('mindirect', 'Minify files directly using UglifyJS.', function() {
-    var files = grunt.file.expandFiles(this.data);
-	files.forEach(function(file) {
-	    var max = grunt.file.read(file);
-	    var min = grunt.helper('uglify', max, grunt.config('uglify'));
-	    grunt.file.write(file, min);
-	    if (this.errorCount) return false;
-	    grunt.log.writeln('File "' + file + '" was minified.');
-	    grunt.helper('min_max_info', min, max);
-	});
-  });
+    grunt.registerMultiTask('mindirect', 'Minify files directly using UglifyJS.', function() {
+        var files = grunt.file.expandFiles(this.data);
+        files.forEach(function(file) {
+            var max = grunt.file.read(file);
+            var min = grunt.helper('uglify', max, grunt.config('uglify'));
+            grunt.file.write(file, min);
+            if (this.errorCount) return false;
+            grunt.log.writeln('File "' + file + '" was minified.');
+            grunt.helper('min_max_info', min, max);
+        });
+    });
 };
